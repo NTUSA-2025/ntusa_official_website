168b5ab27594dd8f827d81c9e8f9e2907d062e20
leo5358 <leo1yang5358@gmail.com>
2026-08-01 21:46:46 +0800

    fix: add cross-origin opener policy

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,10 @@ const nextConfig: NextConfig = {
             value: "max-age=31536000; includeSubDomains",
           },
           {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
             key: "Content-Security-Policy",
             value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com; img-src 'self' data: https: blob:; connect-src 'self' https:;",
           },
